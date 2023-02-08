d8526bd296b07761bd449f6159dfc1d6c2264f14
Daniel Ramirez <danielramirez.wm@gmail.com>
2023-02-07 22:10:12 -0500

    Manejo de estados

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -2,17 +2,22 @@ import React from 'react';
 import '../styles/TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return(
+    return[
         <input 
             className="search" 
             placeholder="Test"
+            value={searchValue}
             onChange={onSearchValueChange} 
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
